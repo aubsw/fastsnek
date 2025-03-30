b50a09b2580cd95f593ace5a4a38e016304caa17
Aubrey Wahl <10409668+aubsw@users.noreply.github.com>
2025-03-30 16:48:01 -0400

    move a ball

--- hello_world.cc
+++ hello_world.cc
@@ -3,13 +3,16 @@
 
 const int screenWidth = 800;
 const int screenHeight = 450;
+Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+const int move_amount = 5.0f;
+
 
 void UpdateDrawFrame(void);     // Update and Draw one frame
 
 int main(void)
 {
-        InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-        emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+        InitWindow(screenWidth, screenHeight, "Hello app.");
+        emscripten_set_main_loop(UpdateDrawFrame, 10, 1);
         CloseWindow();        // Close window and OpenGL context
         return 0;
 }
@@ -19,6 +22,13 @@ void UpdateDrawFrame(void)
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello everybody!!", 190, 200, 20, LIGHTGRAY);
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += move_amount;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= move_amount;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= move_amount;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += move_amount;
+	DrawCircleV(ballPosition, 50, MAROON);
+        DrawFPS(10, 10);
+
         EndDrawing();
 }
 
